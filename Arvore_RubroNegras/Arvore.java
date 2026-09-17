f1b413b77108168491be201de8e5c65ef24f4175
Joao-Pedro <joaopmalvarez@hotmail.com>
2026-09-17 08:23:28 -0300

    Atualizar o Arvore.java

--- Arvore_RubroNegras/Arvore.java
+++ Arvore_RubroNegras/Arvore.java
@@ -2,100 +2,97 @@ public class Arvore {
     private No raiz;
 
     public Arvore() {
-        this.raiz = raiz;
+        this.raiz = null;
     }
 
     private void rotacaoEsq(No x) {
         No y = x.dir;
         x.dir = y.esq;
-
         if (y.esq != null) y.esq.pai = x;
         y.pai = x.pai;
-        if(x.pai == null) raiz = y;
-        else {
-            if (x == x.pai.esq) x.pai.esq = y;
-            else x.pai.dir = y;
-        }
+        if (x.pai == null) raiz = y;
+        else if (x == x.pai.esq) x.pai.esq = y;
+        else x.pai.dir = y;
         y.esq = x;
         x.pai = y;
     }
 
     private void rotacaoDir(No y) {
-        No x =  y.esq;
+        No x = y.esq;
         y.esq = x.dir;
-
         if (x.dir != null) x.dir.pai = y;
         x.pai = y.pai;
-        if(y.pai == null) raiz = x;
-        else {
-            if (y == y.pai.esq) y.pai.esq = x;
-            else y.pai.dir = x;
-        }
+        if (y.pai == null) raiz = x;
+        else if (y == y.pai.esq) y.pai.esq = x;
+        else y.pai.dir = x;
         x.dir = y;
         y.pai = x;
     }
 
-    private void casos(No x) {
-        No avo = x.pai.pai
-        if (avo.esq == x.pai) { // tio na direita
-            No tio = avo.dir;
-        } else No tio = avo.esq; // tio na esquerda
-
-        // caso 1:
-        if (tio.cor == true) { // se tio é vermelho
-            tio.cor = false;
-            x.pai.cor = false;
-            avo.cor = true;
-            x = avo;
-        }
-
-        // caso 2:
-        if (tio.cor == false && x == x.pai.dir) { // se tio é preto e o x é o filho direito
-            x = x.pai;
-            if (tio == null) {
-                rotacaoDir(x);
-                rotacaoEsq(x.pai);
+    private void corrigir(No x) {
+        while (x.pai != null && x.pai.cor == true) { // pai vermelho
+            No avo = x.pai.pai;
+            if (x.pai == avo.esq) {
+                No tio = avo.dir;
+                if (tio != null && tio.cor == true) { // caso 1: tio vermelho
+                    x.pai.cor = false;
+                    tio.cor = false;
+                    avo.cor = true;
+                    x = avo;
+                } else {
+                    if (x == x.pai.dir) { // caso 2: tio preto, x filho direito
+                        x = x.pai;
+                        rotacaoEsq(x);
+                    }
+                    x.pai.cor = false; // caso 3: tio preto, x filho esquerdo
+                    avo.cor = true;
+                    rotacaoDir(avo);
+                }
+            } else { // espelhado: pai é filho direito do avô
+                No tio = avo.esq;
+                if (tio != null && tio.cor == true) { // caso 1
+                    x.pai.cor = false;
+                    tio.cor = false;
+                    avo.cor = true;
+                    x = avo;
+                } else {
+                    if (x == x.pai.esq) { // caso 2
+                        x = x.pai;
+                        rotacaoDir(x);
+                    }
+                    x.pai.cor = false; // caso 3
+                    avo.cor = true;
+                    rotacaoEsq(avo);
+                }
             }
-            rotacaoEsq(x);
         }
-        
-        // caso 3:
-        if (tio.cor == false && x == x.pai.esq) {// se tio é preto e o x é o filho esquerdo
-            x.pai.cor = false;
-            avo.cor = true;
-            if (tio == null) {
-                rotacaoEsq(avo);
-                if (avo.pai != null)    rotacaoDir(avo.pai);
-            }
-            rotacaoDir(avo);
-        }
-
+        raiz.cor = false; // raiz sempre preta
     }
 
     public void inclusao(No x) {
-        if (raiz == null)
-            x = raiz;
-        else {
+        x.cor = true; // novo nó sempre entra vermelho
+        x.esq = null;
+        x.dir = null;
+
+        if (raiz == null) {
+            raiz = x;
+            x.pai = null;
+        } else {
             No p = raiz;
             No q = null;
             while (p != null) {
                 q = p;
-                if (x.chave < p.chave)
-                    p = p.esq;
-                else if (x.chave > p.chave)
-                    p = p.dir;
-                else if (x.chave == p.chave) {
+                if (x.chave < p.chave) p = p.esq;
+                else if (x.chave > p.chave) p = p.dir;
+                else {
                     System.out.printf("Nó de chave %d já existe!\n", x.chave);
                     return;
                 }
             }
-            if (x.chave < q.chave)
-                q.esq = x;
-                x.pai = q;
-            else if (chave > q.chave)
-                q.dir = x;
-                x.pai = q;
-            if (x.pai.cor = false) casos(No x);
+            x.pai = q;
+            if (x.chave < q.chave) q.esq = x;
+            else q.dir = x;
         }
+        corrigir(x);
     }
 }
